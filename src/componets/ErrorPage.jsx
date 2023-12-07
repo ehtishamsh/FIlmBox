d7384abaa45c123c6049be83e49769d0b7fb9807
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-12-07 05:57:09 +0500

    Design error page

--- src/componets/ErrorPage.jsx
+++ src/componets/ErrorPage.jsx
@@ -1,16 +1,34 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function ErrorPage() {
   const location = useLocation();
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/");
+  }
   const errorMessage = location.state?.message || "Lost your way";
   return (
     <div
-      className=" pb-16 flex flex-col items-center justify-center px-10 h-full pt-32  gap-14 max-sm:flex-col bg-cover object-cover"
+      className=" pb-16 flex flex-col items-center justify-center px-10 h-full pt-32  gap-10 max-sm:flex-col bg-cover object-cover"
       style={{ backgroundImage: `url("/Background.png")` }}
     >
       <img src="/error.png" alt="" className="w-full h-full max-w-[400px]" />
-      <h2 className="text-9xl mt-10 text-slate-200">{errorMessage}</h2>
+      <div className="text-center">
+        <h2 className="text-5xl font-bold text-slate-200 mb-5">
+          {errorMessage}
+        </h2>
+        <p className="text-base text-slate-500">
+          Oops! This is awkward. You are looking for something that doesn't
+          actually exist.
+        </p>
+      </div>
+      <button
+        onClick={handleClick}
+        className="py-4 px-8 text-slate-200 transition-colors duration-300 hover:bg-indigo-600 active:bg-indigo-700 bg-indigo-500 rounded-2xl"
+      >
+        Go Home
+      </button>
     </div>
   );
 }
