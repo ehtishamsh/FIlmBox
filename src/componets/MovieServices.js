842ca11d458d53338bb3e8c142551b894ac19283
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-12-07 05:56:46 +0500

    addded search url

--- src/componets/MovieServices.js
+++ src/componets/MovieServices.js
@@ -1,16 +1,19 @@
 const key = import.meta.env.VITE_TMDB_KEY;
 const baseUrl = "https://api.themoviedb.org/3";
 
-const details = (movieID, trend) => {
+const details = (query, trend) => {
   const endpoints = {
     popular: `${baseUrl}/movie/popular?api_key=${key}`,
     topRated: `${baseUrl}/movie/top_rated?api_key=${key}`,
     trending: `${baseUrl}/trending/${trend}/day?api_key=${key}`,
     comedy: `${baseUrl}/search/movie?api_key=${key}&language=en-US&query=comedy&page=1&include_adult=false`,
     upcoming: `${baseUrl}/movie/upcoming?api_key=${key}`,
-    detailsMovie: `${baseUrl}/movie/${movieID}?api_key=${key}`,
+    detailsMovie: `${baseUrl}/movie/${query}?api_key=${key}`,
     popularTv: `${baseUrl}/tv/popular?api_key=${key}`,
-    detailsTV: `${baseUrl}/tv/${movieID}?api_key=${key}`,
+    detailsTV: `${baseUrl}/tv/${query}?api_key=${key}`,
+    searchAll: `${baseUrl}/search/multi?api_key=${key}&query=${query}`,
+    searchMovie: `${baseUrl}/search/movie?api_key=${key}&query=${query}`,
+    searchTV: `${baseUrl}/search/tv?api_key=${key}&query=${query}`,
   };
   return endpoints;
 };
